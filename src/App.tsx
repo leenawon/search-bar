e2f408204e801c9ce1d99b5026d77e76f109ef72
leenawon <nawon5154@gmail.com>
2022-02-18 23:55:21 +0900

    Modify - App.tsx에 SearchBar 컴포넌트 추가 및 css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import SearchBar from './components/SearchBar/SearchBar';
+import bookList from './data/data.json';
 import './Reset.css';
+import './App.css';
 
 function App() {
   return (
-    <>
-      Search Bar
-    </>
+    <div className='app'>
+      <h1 className="title">Book Search Bar</h1>
+      <SearchBar placeholder='책 이름을 입력하세요' bookList={bookList} />
+    </div>
   );
 }
 
